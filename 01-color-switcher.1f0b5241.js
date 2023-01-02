1dc860d1efec0bfc83a4d8abe64d550c595c3f15
Viktorr22 <110117605+Viktorr22@users.noreply.github.com>
2023-01-02 11:10:39 +0000

    Deploying to gh-pages from @ Viktorr22/goit-js-hw-09@3a738946cf57a80bd5ec1739c9abe1d3ec3cbb7a 🚀

---
+++ 01-color-switcher.1f0b5241.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;function n(){return r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{o=setInterval(n,1e3),t.setAttribute("disabled","disabled")}));e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.1f0b5241.js.map
