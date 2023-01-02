3a738946cf57a80bd5ec1739c9abe1d3ec3cbb7a
Viktorr22 <110117605+Viktorr22@users.noreply.github.com>
2023-01-02 13:10:05 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+
+function getRandomHexColor() {
+  return body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startChangeColor = () => {
+    timerId = setInterval(getRandomHexColor, 1000);
+    buttonStart.setAttribute("disabled","disabled");
+}
+
+buttonStart.addEventListener('click', startChangeColor);
+
+const stopChangeColor = () => {
+    clearInterval(timerId);
+    buttonStart.removeAttribute("disabled");
+}
+
+buttonStop.addEventListener('click', stopChangeColor);
+
 
